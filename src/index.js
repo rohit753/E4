3d0e7cc2ab1fb18c9de7e9d37b245a71b8ade12a
Rohit Kumar Sahu <rohit753kumar@gmail.com>
2022-02-21 17:16:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom"
-import { createStore } from "redux"
-import { composeWithDevTools } from "redux-devtools-extension"
-import { Provider } from "react-redux"
+// import { createStore } from "redux"
+// import { composeWithDevTools } from "redux-devtools-extension"
+// import { Provider } from "react-redux"
 
 // const store = createStore(SearchReducer,composeWithDevTools)
 
